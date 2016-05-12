cba41dcbe4f91409a21f35c09ed3011a7848f2dd
Krav20 <axn99tv@gmail.com>
2016-05-12 16:50:06 +0200

    -Task #552494 Zapis do pliku ical. -Ogólna poprawa kodu oraz uzupełnienie dokumentacji

--- beany/SaveIcal.java
+++ beany/SaveIcal.java
@@ -1,13 +1,10 @@
 package beany;
 
-import java.beans.EventSetDescriptor;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
@@ -18,41 +15,59 @@ import klasy.Event;
 // TODO: Auto-generated Javadoc
 /**
  * The Class SaveIcal.
+ * @author Krav(Przemys³aw Krawczel)
  */
 @ManagedBean
 @SessionScoped
 public class SaveIcal {
-	
+
 	/** The version. */
-	private String version = "VERSION:2.0\r\n";
-	
+	private static final String VERSION = "VERSION:2.0\r\n";
+
 	/** The prodid. */
-	private String prodid = "PRODID://21INF-SP//Google Calendar GENERATOR\r\n";
-	
+	private static final String PRODID = "PRODID://21INF-SP//Google Calendar GENERATOR\r\n";
+
 	/** The cal begin. */
-	private String calBegin = "BEGIN:VCALENDAR\r\n";
-	
+	private static final String CALBEGIN = "BEGIN:VCALENDAR\r\n";
+
 	/** The cal end. */
-	private String calEnd = "END:VCALENDAR\r\n";
-	
+	private static final String CALEND = "END:VCALENDAR\r\n";
+
 	/** The event begin. */
-	private String eventBegin = "BEGIN:VEVENT\r\n";
-	
+	private static final String EVENTBEGIN = "BEGIN:VEVENT\r\n";
+
 	/** The event end. */
-	private String eventEnd = "END:VEVENT\r\n";
-	
+	private static final String EVENTEND = "END:VEVENT\r\n";
+
 	/** The date start. */
-	private String dateStart = "DTSTART:";
-	
+	private static final String DATESTART = "DTSTART:";
+
+	/** The Constant DATEEND. */
+	private static final String DATEEND = "DTEND:";
+
 	/** The begin v timezone. */
-	private String beginVTimezone = "BEGIN:VTIMEZONE\r\n";
+	private static final String BEGINVTIMEZONE = "BEGIN:VTIMEZONE\r\n";
+
+	/** The Constant DESC. */
+	private static final String DESC = "DESCRIPTION:";
+
+	/** The Constant EVENTTITLE. */
+	private static final String EVENTTITLE = "SUMMARY:";
+
+	/**
+	 * Instantiates a new save ical.
+	 */
+	public SaveIcal() {
+		final int consta = 0;
+	}
 
 	/**
 	 * Save.
 	 *
-	 * @param events the events
+	 * @param events
+	 *            the events
 	 */
-	public void save(List<Event> events) {
+	public void save(final List<Event> events) {
 		if (!isEventEmpty(events)) {
 			saveToFile(events);
 		}
@@ -61,28 +76,33 @@ public class SaveIcal {
 	/**
 	 * Checks if is event empty.
 	 *
-	 * @param events the events
+	 * @param events
+	 *            the events
 	 * @return true, if is event empty
 	 */
-	private boolean isEventEmpty(List<Event> events) {
+	private boolean isEventEmpty(final List<Event> events) {
+		boolean isEmpty1;
 		if (events.isEmpty()) {
+			isEmpty1 = true;
 			System.out.println("There is nothing to save.");
-			return true;
+
+		} else {
+			isEmpty1 = false;
 		}
-		return false;
+		return isEmpty1;
 
 	}
 
 	/**
 	 * Save to file.
 	 *
-	 * @param events the events
+	 * @param events
+	 *            the events
 	 */
-	private void saveToFile(List<Event> events) {
-		StringBuilder fileIcal = new StringBuilder();
-		fileIcal.append("ical");
-		fileIcal.append(".ics");
-		File file = new File("D:/ical.ics");
+	private void saveToFile(final List<Event> events) {
+		final StringBuilder fileIcal = new StringBuilder();
+		fileIcal.append("ical.ics");
+		final File file = new File("D:/ical.ics");
 		if (!file.exists()) {
 			try {
 				file.createNewFile();
@@ -92,20 +112,24 @@ public class SaveIcal {
 			}
 		}
 		try {
-			FileWriter iCalFile = new FileWriter(file.getAbsoluteFile());
-			BufferedWriter buf = new BufferedWriter(iCalFile);
-			buf.write(calBegin);
-			buf.write(version);
-			buf.write(prodid);
-			buf.write(beginVTimezone);
+			final FileWriter iCalFile = new FileWriter(file.getAbsoluteFile());
+			final BufferedWriter buf = new BufferedWriter(iCalFile);
+			buf.write(CALBEGIN);
+			buf.write(VERSION);
+			buf.write(PRODID);
+			buf.write(BEGINVTIMEZONE);
+			buf.write("TZID:Europe/Warsaw\r\n");
 			for (int i = 0; i < events.size(); i++) {
-				buf.write(eventBegin);
+				buf.write(EVENTBEGIN);
 
-				buf.write(dateStart + dateParse(events.get(i)));
-				buf.write(eventEnd);
+				buf.write(DATESTART + dateParse(events.get(i), true));
+				buf.write(DATEEND + dateParse(events.get(i), false));
+				buf.write(EVENTTITLE + getTitle(events.get(i)) + "\r\n");
+				buf.write(DESC + getDescription(events.get(i)) + "\r\n");
+				buf.write(EVENTEND);
 				System.out.println("stworzono");
 			}
-			buf.write(calEnd);
+			buf.write(CALEND);
 			buf.close();
 
 		} catch (IOException e) {
@@ -116,21 +140,48 @@ public class SaveIcal {
 	}
 
 	/**
-	 * Date parse.
+	 * Gets the title.
 	 *
 	 * @param event the event
+	 * @return the title
+	 */
+	private String getTitle(final Event event) {
+		return event.getTitle();
+	}
+
+	/**
+	 * Gets the description.
+	 *
+	 * @param event the event
+	 * @return the description
+	 */
+	private String getDescription(final Event event) {
+		return event.getDescription();
+	}
+
+	/**
+	 * Date parse.
+	 *
+	 * @param event
+	 *            the event
+	 * @param startOrEnd
+	 *            the start or end
 	 * @return the string
 	 */
-
-	private String dateParse(Event event) {	
+	private String dateParse(final Event event, final boolean startOrEnd) {
+		final StringBuilder formated = new StringBuilder();
 		SimpleDateFormat format1 = new SimpleDateFormat("yyyyMMdd");
-		String formated = format1.format(event.getStartDate());
-		formated +="T";
+		if (startOrEnd) {
+			formated.append(format1.format(event.getStartDate()));
+		} else {
+			formated.append(format1.format(event.getEndDate()));
+		}
+		formated.append('T');
 		format1 = new SimpleDateFormat("HHmmss");
-		formated += format1.format(event.getStartDate());
-		formated += "\r\n";
+		formated.append(format1.format(event.getStartDate()));
+		formated.append("\r\n");
 		System.out.println(formated);
-		return formated;
+		return formated.toString();
 	}
-	
+
 }
