66d039cc94c6635366625a7f2b9e0393efbe2df2
Sebastian Podgórski <sebastian.podgorski94@gmail.com>
2016-05-13 00:07:47 +0200

    Dodanie funkcji kopiowania eventu Feature #553377 oraz drobne poprawki.
    Takie jak:
    -poprawa dokumentacji
    -poprawa overLay panel, juz sie nie zamyka podczas podawania dat
    -stworzenie konstruktorow w klasie event

--- ICall/src/beany/EventBean.java
+++ ICall/src/beany/EventBean.java
@@ -8,24 +8,39 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import klasy.Event;
 
+
 /**
- * @author Wenaro
+ * The Class EventBean.
  *
+ * @author Wenaro
  */
 @ManagedBean
 @SessionScoped
 public class EventBean implements Serializable {
 
+	/** The Constant serialVersionUID. */
 	private static final long serialVersionUID = 1L;
+	
+	/** The event. */
 	private Event event;
+	
+	/** The events. */
 	private List<Event> events;
 
+	/**
+	 * Inits the event and events.
+	 */
 	@PostConstruct
 	public void init() {
 		event = new Event();
 		events = new ArrayList<Event>();
 	}
-
+	
+	/**
+	 * Contain.
+	 *
+	 * @return true, if successful
+	 */
 	public boolean contain() {
 		boolean isOnList = false;
 		for (Event eventf : events) {
@@ -34,6 +49,10 @@ public class EventBean implements Serializable {
 		return isOnList;
 	}
 
+
+	/**
+	 * Adds the event.
+	 */
 	public void add() {
 		if (contain()) {
 			clearEvent();
@@ -43,45 +62,91 @@ public class EventBean implements Serializable {
 		}
 	}
 
+	/**
+	 * Clear event.
+	 */
 	public void clearEvent() {
 		event = new Event();
 	}
-
+	
+	/**
+	 * Clear list of events.
+	 */
 	public void clear() {
 		events.removeAll(events);
 	}
 
+	/**
+	 * Removes the event.
+	 *
+	 * @param event the event
+	 * @return the string
+	 */
 	public String remove(Event event) {
 		events.remove(event);
 		return null;
 	}
 
-	public String saveAction() {
+	/**
+	 * Save action.
+	 */
+	public void saveAction() {
 		for (Event event : events) {
 			event.setEditable(false);
 		}
-		return null;
 	}
 
-	public String edit(Event event) {
+	/**
+	 * Set editable for event.
+	 *
+	 * @param event the event
+	 */
+	public void edit(Event event) {
 		event.setEditable(true);
-		return null;
 	}
 
+	/**
+	 * Copy event.
+	 *
+	 * @param event the event
+	 */
+	public void copy(Event event){
+		events.add(new Event(event));
+	}
+	
+	/**
+	 * Gets the event.
+	 *
+	 * @return the event
+	 */
 	public Event getEvent() {
 		return event;
 	}
 
+	/**
+	 * Gets the events.
+	 *
+	 * @return the events
+	 */
 	public List<Event> getEvents() {
 		return events;
 	}
 
+	/**
+	 * Sets the event.
+	 *
+	 * @param event the new event
+	 */
 	public void setEvent(Event event) {
 		this.event = event;
 	}
 
+	/**
+	 * Sets the events.
+	 *
+	 * @param events the new events
+	 */
 	public void setEvents(List<Event> events) {
 		this.events = events;
 	}
-
 }
